6e66cf1170fe827575a7e7deae04ff5ec536822d
KumudaTY <88778308+KumudaTY@users.noreply.github.com>
2022-07-05 10:32:59 +0530

    Update fire-flink.py

--- fire-flink.py
+++ fire-flink.py
@@ -18,7 +18,7 @@ def login(token):
         "Content-Type": "application/json"
     }
     head["Authorization"] = "Bearer " + token
-    suiteid = 'SUITE1057'
+    suiteid = 'SUITE1006'
     pes = s.post('https://app.flinko.com:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
